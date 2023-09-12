87191550ddfaec6d45c6d05eb01e7f8cc1386383
재 현 <62232660+songjayhyun@users.noreply.github.com>
2023-09-12 18:05:57 +0900

    [Silver II] Title: N번째 큰 수, Time: 1652 ms, Memory: 33324 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.py
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.py
@@ -1,16 +1,12 @@
-import heapq
-
-N = int(input())
-table = []
-
-# N x N 표의 수를 읽어와서 heapq에 저장
-for _ in range(N):
-    row = list(map(int, input().split()))
-    for num in row:
-        heapq.heappush(table, num)
-        # table에는 현재까지 가장 작은 N개의 수가 저장됩니다.
-        if len(table) > N:
-            heapq.heappop(table)
-
-# N번째로 큰 수 출력
-print(table[0])
+import sys
+from heapq import heappush, heappop
+n = int(sys.stdin.readline().rstrip())
+h = []
+for _ in range(n):
+    c = list(map(int, sys.stdin.readline().split()))
+    for i in c:
+        heappush(h, i)
+    if len(h) > n:        
+        for i in range(n):
+            heappop(h)
+print(h[0])
